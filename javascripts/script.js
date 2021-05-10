03ec630ffa98a56a616b4aa4dbd6eae038544108
Evgeniy3171 <evgen915628@list.ru>
2021-05-10 21:47:51 +0300

    Закончил урок 1

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+//Работа с ипутами
+
+//Чекбокс
+/* const one = document.getElementById('one');
+
+one.addEventListener('change', function(){
+    if (one.checked){
+        alert('Чек выбран');
+    } else {
+        alert('Чек не выбран');
+    }
+    }
+); */
+
+//радиобаттон
+const one = document.getElementsByName('r1'),
+      btn = document.getElementById('Validated');
+
+btn.addEventListener('click', function(){
+    for (let i = 0; i < one.length; i++){
+    if (one[i].checked){
+        alert('Выбран ' + i + ' элемент');
+    }}
+});
+
+//Закончил урок 1 - Инпуты
